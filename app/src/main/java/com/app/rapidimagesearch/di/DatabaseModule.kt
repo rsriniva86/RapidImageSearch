082d771ef512c498440066387b064dee077a6e9d
rsriniva86 <radheshyam.srinivasan@gmail.com>
2021-07-25 17:49:51 +0800

    add di class for database

---
+++ app/src/main/java/com/app/rapidimagesearch/di/DatabaseModule.kt
@@ -0,0 +1,33 @@
+package com.app.rapidimagesearch.di
+
+import android.content.Context
+import androidx.room.Room
+import com.app.rapidimagesearch.db.DatabaseService
+import com.app.rapidimagesearch.db.ImageDataDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@InstallIn(ApplicationComponent::class)
+@Module
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideImageDao(databaseService: DatabaseService): ImageDataDao {
+        return databaseService.imageDao()
+    }
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext appContext: Context): DatabaseService {
+        return Room.databaseBuilder(
+            appContext,
+            DatabaseService::class.java,
+            "RapidImageSearch"
+        ).build()
+    }
+}
